f3c62e402ed6d0c44675719e07a399bc50d8b31b
David Wu <david204code@gmail.com>
2021-02-16 15:21:05 +0000

    Calculate total example and used functions, return, if, arrays and for loop

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-// Arrays and for loop
+// Functions, return, if, arrays, for loop
 
-const names = ["Peter", "James", "John", "Mark"];
-const lastName = "Disciple";
-let newArray = [];
+const gas = [20, 40, 100, 30];
+const food = [10, 40, 50];
 
-// for loop
-for (let i = 0; i < names.length; i++) {
-  console.log(i);
-  console.log(names[i]);
-  // newArray.push(`${names[i]} ${lastName}`);
-  const fullName = `${names[i]} ${lastName}`;
-  newArray.push(fullName);
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    total += arr[i];
+  }
+  if (total > 100) {
+    console.log(`Whoa overspending!`);
+    // break out of the function by setting up a return inside this if statemetn
+    return total;
+  }
+  console.log(`Total is less than 100`);
+  return total;
+  // console.log(total);
 }
 
-console.log(names);
-console.log(newArray);
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+const randomTotal = calculateTotal([2004, 2008, 2009, 2010, 2013]);
+
+// pass in object for console log
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+  random: randomTotal,
+});
