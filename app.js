71452d8fb83ccc3597ceb372cbcdc2f78ac6a8e4
David Wu <david204code@gmail.com>
2021-02-17 23:46:15 +0000

    reduce() method example

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-// find function
-// returns single instances/single object
-// return first match, if no match than undefined
-// great for getting unique value
+// reduce method
+// still iterates over the array, callback function, 2 parameters
+// reduces the value in the array to a single value = number, array, object
+// in the callback function 1st parameter ('acc') - total of all calculation
+// in the call back funtion 2nd paramter ('curr') - current iteration/value
 
 const people = [
-  { name: "David", age: 26, position: "Worshipper", id: 1 },
-  { name: "Wu", age: 23, position: "Believer", id: 2 },
-  { name: "David Wu", age: 15, position: "Child of God", id: 3 },
-  { name: "Mr David Wu", age: 19, position: "Seeker", id: 4 },
+  { name: "David", age: 26, position: "Worshipper", id: 1, salary: 200 },
+  { name: "Wu", age: 23, position: "Believer", id: 2, salary: 300 },
+  { name: "David Wu", age: 15, position: "Child of God", id: 3, salary: 500 },
+  { name: "Mr David Wu", age: 19, position: "Seeker", id: 4, salary: 500 },
 ];
 
-const names = ["Matthew", "Mark", "Luke"];
+const total = people.reduce(
+  function (acc, currItem) {
+    console.log(`total: ${acc}`);
+    console.log(`current money : ${currItem.salary} `);
 
-console.log(
-  names.find(function (name) {
-    return name === "Matthew";
-  })
+    // the functionality
+    acc += currItem.salary;
+
+    // must alway return acc, the first total
+    return acc;
+  },
+  // initial value in the loop
+  0
 );
 
-const person = people.find(function (person) {
-  // add the condition
-  return person.id === 4;
-});
-
-console.log(person);
-
-const person1 = people.filter(function (person) {
-  // add the condition
-  return person.id === 4;
-});
-
-console.log(person1[0]);
+console.log(total);
