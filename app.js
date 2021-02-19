1d6a786a67b8ea063572210e6376071a9e184fca
David Wu <david204code@gmail.com>
2021-02-19 23:04:40 +0000

    getElementByClassName example

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-// getElementByTagName
+// getElementByClassName
 
-// getElementByTagName('tagname')
+// getElementByClassName('classname')
 
-const headings = document.getElementsByTagName("h2");
-headings[0].style.color = "red";
-// console.log(headings.length);
+const listItems = document.getElementsByClassName("special");
 
-const items = document.getElementsByTagName("li");
-
-// // wouldn't work!
-// items.forEach(function (item) {
-//   console.log(item);
-// });
-
-// transform nodelist to an array for forEach to work
-const betterItems = [...items];
-items[3].style.color = "red";
-
-betterItems.forEach(function (item) {
-  // console.log(item);
-});
-
-console.log(items);
-console.log(betterItems);
-
-document.getElementById("btn").style.backgroundColor = "blue";
-document.getElementById("btn").style.color = "white";
+// console.log(listItems);
+listItems[3].style.color = "red";
