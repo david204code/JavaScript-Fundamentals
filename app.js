a6b64205e578efa282a303d79b5619716d583b08
David Wu <david204code@gmail.com>
2021-02-19 22:58:44 +0000

    getElementByTagName example and tranform nodelist to an array example to use forEach()

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-// getElementById method
+// getElementByTagName
 
-// getElementById('element')
+// getElementByTagName('tagname')
 
-const h1 = document.getElementById("title");
-h1.style.color = "red";
+const headings = document.getElementsByTagName("h2");
+headings[0].style.color = "red";
+// console.log(headings.length);
+
+const items = document.getElementsByTagName("li");
+
+// // wouldn't work!
+// items.forEach(function (item) {
+//   console.log(item);
+// });
+
+// transform nodelist to an array for forEach to work
+const betterItems = [...items];
+items[3].style.color = "red";
+
+betterItems.forEach(function (item) {
+  // console.log(item);
+});
+
+console.log(items);
+console.log(betterItems);
 
 document.getElementById("btn").style.backgroundColor = "blue";
 document.getElementById("btn").style.color = "white";
