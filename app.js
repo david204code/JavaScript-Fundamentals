4996729b8dfd97afcc776b3e229767d14d19d203
David Wu <david204code@gmail.com>
2021-02-23 15:57:27 +0000

    Access the chidNotes example - children, firstChild & lastChild

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-// QuerySelector and QuerySelectorAll
-// Select the element or group of elements
-// Decide the effect we want to apply to the selection
+// nagivate the DOM children
 
-// querySelector('any css'); selects single, selecting the first element
-// querySelectorAll('any css') - select all, the whole list can use the forEach() method
+// childNodes
+// children
+// firstChild
+// lastChild
 
-// # for id and . for class
 const result = document.querySelector("#result");
-result.style.backgroundColor = "blue";
 
-const item = document.querySelector(".special");
-// console.log(item);
+// select the children, use the childNodes and childNotes return whitespace which is treated as text node
+const allChildren = result.childNodes;
+console.log(allChildren);
 
-const lastItem = document.querySelector("li:last-child");
-// console.log(lastItem);
+// children, take out the whitespace
+const children = result.children;
+console.log(children);
 
-const list = document.querySelectorAll(".special");
-// console.log(list);
-list.forEach(function (item) {
-  console.log(item);
-  item.style.color = "yellow";
-});
+// firstChild - still will have the whitespace as text node
+console.log(result.firstChild);
+// lastChild - still will have the whitespace as text node
+console.log(result.lastChild);
