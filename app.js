7f711e43a03d57f68f75db3a649470698824ce37
David Wu <david204code@gmail.com>
2021-02-24 00:33:10 +0000

    getAttribute() & setAttribute(), which dynamically add class and text content

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-// nodeValue
-// textContent
+// getAttribute()
+// setAttribute()
 
-const item = document.getElementById("special");
-const value = item.firstChild.nodeValue;
-// console.log(value);
+// GET THE ATTRIBUTE
+// const first = document.querySelector(".first");
+// // console.log(first);
+// const classValue = first.getAttribute("class");
+// console.log(classValue);
+// const idValue = first.getAttribute("id");
+// console.log(idValue);
 
-const easyValue = item.textContent;
-console.log(easyValue);
+// const link = document.getElementById("link");
+// // console.log(link);
+// const showLink = link.getAttribute("href");
+// console.log(showLink);
+
+// SET THE ATTRIBUTE, dynamically add class and text content
+const last = link.nextElementSibling;
+last.setAttribute("class", "first");
+last.textContent = "Your glory Lord is what our hearts long for";
+console.log(last.textContent);
+
+const links = document.querySelectorAll(".first");
+console.log(links);
