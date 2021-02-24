2c5a12f30bd171671ffb09894a9d7d2d44b522b5
David Wu <david204code@gmail.com>
2021-02-24 23:37:16 +0000

    className, classList example using add, remove & contain property

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-// getAttribute()
-// setAttribute()
+// className allows us to check for the class name and add or edit the class
+// classList
 
-// GET THE ATTRIBUTE
-// const first = document.querySelector(".first");
-// // console.log(first);
-// const classValue = first.getAttribute("class");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+const classValue = first.className;
 // console.log(classValue);
-// const idValue = first.getAttribute("id");
-// console.log(idValue);
 
-// const link = document.getElementById("link");
-// // console.log(link);
-// const showLink = link.getAttribute("href");
-// console.log(showLink);
+// add class (className) to the element
+// second.className = "colors";
+// // this will over written previous assignment
+// second.className = "text";
+// can use both side by side
+// second.className = "colors text";
 
-// SET THE ATTRIBUTE, dynamically add class and text content
-const last = link.nextElementSibling;
-last.setAttribute("class", "first");
-last.textContent = "Your glory Lord is what our hearts long for";
-console.log(last.textContent);
+// classList can be used AND need to use the "add" property
+const classValue1 = third.classList;
+// third.classList.add("colors");
+// third.classList.add("text");
+third.classList.add("text", "colors");
+console.log(classValue1);
 
-const links = document.querySelectorAll(".first");
-console.log(links);
+// removeClasses
+third.classList.remove("text");
+// third.classList.remove("text", "");
+console.log(classValue1);
+
+// contain - check if the elements does have that class
+let result = third.classList.contains("text");
+if (result) {
+  console.log("Ask and it will be given to you");
+} else {
+  console.log("You have not because you ask will evil intent");
+}
