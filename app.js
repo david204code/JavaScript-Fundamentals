a17a62ff6aa660ac9ee40be12d3de0e7279b2a1c
David Wu <david204code@gmail.com>
2021-02-25 21:34:21 +0000

    Change CSS with the STYLE property example

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// inner html
-// textContent
+// CSS
 
-const list = document.getElementById("first");
-const div = document.getElementById("second");
-const item = document.querySelector(".item");
+const random = document.querySelector(".random");
 
-console.log(div.textContent);
-console.log(list.innerHTML);
-console.log(list.innerText);
-console.log(list.textContent);
+// random.style.backgroundColor = "blue";
+// random.style.color = "white";
+// random.style.fontSize = "3rem";
+// random.style.textTransform = "capitalize";
 
-const ul = document.createElement("ul");
-ul.innerHTML = `<li class="item">Joshua</li> <li>Judges</li> <li>Ruth</li>`;
-document.body.appendChild(ul);
+// add the class
+random.classList.add("title");
