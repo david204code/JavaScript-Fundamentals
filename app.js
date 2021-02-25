ecb053f3701a5df054b003b76633d6d8dfb62e55
David Wu <david204code@gmail.com>
2021-02-25 19:27:35 +0000

    createElement(),createTextNode() and element.appenChild() example

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-// className allows us to check for the class name and add or edit the class
-// classList
+// createElement('element')
+// createTextNode('text content')
+// element.appendChild(childElement)
 
-const first = document.getElementById("first");
-const second = document.getElementById("second");
-const third = document.getElementById("third");
+const result = document.querySelector("#result");
 
-const classValue = first.className;
-// console.log(classValue);
+// create a new empty element with the tag name div
+const bodyDiv = document.createElement("div");
+// create node within div
+const text = document.createTextNode("Have your identify in Chirst");
+// place text within bodyDiv
+bodyDiv.appendChild(text);
+document.body.appendChild(bodyDiv);
 
-// add class (className) to the element
-// second.className = "colors";
-// // this will over written previous assignment
-// second.className = "text";
-// can use both side by side
-// second.className = "colors text";
+// console.log(result.children);
+const heading = document.createElement("h2");
+const headingText = document.createTextNode("Gospel, the Good News");
+heading.appendChild(headingText);
+// add the class
+heading.classList.add("blue");
+result.appendChild(heading);
 
-// classList can be used AND need to use the "add" property
-const classValue1 = third.classList;
-// third.classList.add("colors");
-// third.classList.add("text");
-third.classList.add("text", "colors");
-console.log(classValue1);
-
-// removeClasses
-third.classList.remove("text");
-// third.classList.remove("text", "");
-console.log(classValue1);
-
-// contain - check if the elements does have that class
-let result = third.classList.contains("text");
-if (result) {
-  console.log("Ask and it will be given to you");
-} else {
-  console.log("You have not because you ask will evil intent");
-}
+console.log(result.children);
