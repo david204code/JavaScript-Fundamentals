78416a2e0c0685b8a3cfd87a0f0aaa42a00e3a06
David Wu <david204code@gmail.com>
2021-02-27 23:43:35 +0000

    click example, passed in as reference and add toggle functionality

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const btn = document.querySelector(".btn");
 // select the heading
 const heading = document.querySelector("h2");
 
-// first: pass in the event, second: pass in the call back function
-btn.addEventListener("click", function () {
-  // console.log("The best decision");
-  // add a class when the button is click
-  heading.classList.add("red");
-});
+function changeColors() {
+  // console.log("New life, spring of living water");
+  // does the heading contains the class of red
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    // remove the class
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
+// // first: pass in the event, second: pass in the call back function
+// btn.addEventListener("click", function () {
+//   // console.log("The best decision");
+//   // add a class when the button is click
+//   heading.classList.add("red");
+// });
+
+// passing in as reference
+btn.addEventListener("click", changeColors);
