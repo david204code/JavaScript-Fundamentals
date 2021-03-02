f9a310d522ae8fe33248b00326df4244dba4b23a
David Wu <david204code@gmail.com>
2021-03-02 14:43:59 +0000

    Event object example

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-// keypress - when key is pressed
-// keydown - when key is down
-// keyup - when key is released
+// event object argument e,event
+// info about triggered event
+// event.type
+// event.currentTarget
+// this keyword
+// preventDefault() - prevents default behaviour
 
-const nameInput = document.getElementById("name");
+const heading = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+const link = document.getElementById("link");
 
-// nameInput.addEventListener("keypress", function () {
-//   console.log("keypress");
-// });
-
-// nameInput.addEventListener("keydown", function () {
-//   console.log("keydown");
-// });
-
-nameInput.addEventListener("keyup", function () {
-  // console.dir(nameInput);
-  console.log(nameInput.value);
+heading.addEventListener("click", function (event) {
+  // heading.classList.add("blue");
+  console.log(event.currentTarget);
+  console.log(this);
 });
+
+btn.addEventListener("click", function (event) {
+  event.currentTarget.classList.add("blue");
+  console.log(event.type);
+});
+
+function example(e) {
+  e.preventDefault();
+}
+link.addEventListener("click", example);
