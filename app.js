a6349ce9fc2ca22a2dd00e1e13834eaa02418806
David Wu <david204code@gmail.com>
2021-03-02 15:50:17 +0000

    event propogation example: event bubbling is event going up in order, event capturing is event going down in order

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-// currentTarget - always refers to the element to which the event handler has been attached to
-// target - identifies the element on which the event occured
+// allows select dynamic elements
+// event propogation - order the events are fired
+// event bubbling - clicked element first then bubbles up -- default
+// event capturing - fires at the root and fires until reaches target
 
-const btn = document.querySelectorAll(".btn");
+const container = document.querySelector(".container");
+const list = document.querySelector(".list-items");
 
-btn.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    // console.log(e.currentTarget);
-    // e.currentTarget.style.color = "green";
-    console.log("currentTarget", e.currentTarget);
-    console.log("target", e.target);
-    e.target.style.color = "green";
-  });
-});
+function showBubbling(e) {
+  console.log("currentTarget: ", e.currentTarget);
+  // console.log("target", e.target);
+  // if (e.target.classList.contains("link")) {
+  //   console.log("This is a link");
+  // }
+}
+
+// stop propogation
+function stopPropagantion(e) {
+  console.log("clicks on the list");
+  e.stopPropagation();
+}
+
+// list.addEventListener("click", stopPropagantion);
+list.addEventListener("click", showBubbling, { capture: true });
+container.addEventListener("click", showBubbling, { capture: true });
+document.addEventListener("click", showBubbling, { capture: true });
+window.addEventListener("click", showBubbling, { capture: true });
