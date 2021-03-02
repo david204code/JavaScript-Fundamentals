13a7b82cd0f7f16a4d5273df19ab14eecc8d1e55
David Wu <david204code@gmail.com>
2021-03-02 21:29:32 +0000

    JavaScript fundamentals completed - final commit with more example of localStorage with multiple values

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ localStorage.setItem("friends", JSON.stringify(friends));
 
 const values = JSON.parse(localStorage.getItem("friends"));
 console.log(values[0]);
+
+let fruits;
+
+if (localStorage.getItem("fruits")) {
+  fruits = JSON.parse(localStorage.getItem("fruits"));
+} else {
+  fruits = [];
+}
+
+console.log(fruits);
+// fruits.push("apple");
+fruits.push("orange");
+localStorage.setItem("fruits", JSON.stringify(fruits));
